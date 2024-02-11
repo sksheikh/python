5b7e234a64b3fcf0185e5bbcf8572fd82298e14f
Skuddin <sheikh@nextgenitltd.com>
2024-02-11 16:20:47 +0600

    indentation & comment

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print('hello world!')
+# print('hello world!')
+
+# indentation
+# if 5 > 2:
+#   print("Five is greater than two")
+#   print("Five is greater than two")
+
+# variable
+x = 5
+print(x)
