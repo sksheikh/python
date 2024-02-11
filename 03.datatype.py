e80592146e77fd7e18bac5f8b03e76d4078e70d1
Skuddin <sheikh@nextgenitltd.com>
2024-02-11 18:00:00 +0600

    data type

---
+++ 03.datatype.py
@@ -0,0 +1,50 @@
+# different data types
+
+#text type: str
+# x = "Sheikh Salah Uddin"
+# x = str("Sheikh Salah Uddin")
+# print(x)
+# print(type(x))
+
+# numeric types: int, float, complex
+# x = int(20) #int
+# x = float(20.5) #float
+# x = complex(20j) #complex
+# print(type(x))
+
+# sequence types: list, tuple, range
+# x = ["apple", "orange", "banana"] #list
+# x = list(("apple", "orange", "ban")) #list
+# x = ("apple", "orange", "banana") #tuple
+# x = tuple(("apple", "orange", "banana")) #tuple
+# x = range(6) #range
+# print(x)
+# print(type(x))
+
+# mapping types: dict
+# x = {"name" : "sheikh", "age" : 28} #dict
+# x = dict(name = "sheikh", age = 28)
+# print(x)
+# print(type(x))
+
+# set types: set, frozenset
+# x = {"apple", "orange", "banana"} #set
+# x = set(("apple", "orange", "banana")) #set
+# x = frozenset(("apple", "orange", "banana")) #frozenset
+# print(x)
+# print(type(x))
+
+# boolean type: bool
+# x = True #bool
+# x = bool(5 == 4) #bool
+# print(x)
+# print(type(x))
+
+# binary type: bytes, bytearray, memoryview
+
+# none type: NoneType
+
+
+# print data type
+# x = 10
+# print(type(x))
