d1260d6749b64a8c8c78d01dc5ff4918ff729251
Skuddin <sheikh@nextgenitltd.com>
2024-02-12 17:13:19 +0600

    slice string

--- 05.string.py
+++ 05.string.py
@@ -16,13 +16,17 @@
 # print(len(a))
 
 #check string
-txt = "The best things in life are free!"
+# txt = "The best things in life are free!"
 # if 'frees' in txt:
 #   print("Yes, 'free is present here")
 # else:
 #   print("No, 'free is not present here")
 
-if 'free' not in txt:
-  print("No, 'free is not present here")
-else:
-  print("Yes, 'free is present here")
+# if 'free' not in txt:
+#   print("No, 'free is not present here")
+# else:
+#   print("Yes, 'free is present here")
+
+#slicing string
+x = "sheikh"
+print(x[-4:-1])
