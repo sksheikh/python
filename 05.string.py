696f935957d8b23c836e300d657a5f1fd37c9852
Skuddin <sheikh@nextgenitltd.com>
2024-02-12 11:56:56 +0600

    string array, loop, check a specific phrase or character

---
+++ 05.string.py
@@ -0,0 +1,28 @@
+#python string
+# x = "Sheikh Salah uddin" #singleline string
+# y = """Hello there, 
+# I am here"""
+# print(x[1])
+# print(y)
+
+#string array
+
+#for loop
+# for i in x:
+#   print(i)
+
+# length
+# a = "sheikh"
+# print(len(a))
+
+#check string
+txt = "The best things in life are free!"
+# if 'frees' in txt:
+#   print("Yes, 'free is present here")
+# else:
+#   print("No, 'free is not present here")
+
+if 'free' not in txt:
+  print("No, 'free is not present here")
+else:
+  print("Yes, 'free is present here")
