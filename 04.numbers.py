69904f433ad6be4351029f619d63f371d3bbabba
Skuddin <sheikh@nextgenitltd.com>
2024-02-12 10:30:27 +0600

    python numbers and its conversion

---
+++ 04.numbers.py
@@ -0,0 +1,27 @@
+#python number
+
+# 3 numeric type
+#int - positive, negative without decimal point
+# x = 1
+# y = 2454566787
+# z = -235455
+
+#float- negetaive, positive with one or more decimal points
+# x = 1.10
+# x = 2e2 
+# print(x)
+# print(type(x))
+
+#comlex- with 'j'
+# x =  3j + 5
+# print(x)
+# print(type(x))
+
+#convert
+x = 1 #int
+a = float(x) #float
+b = complex(x) #float
+
+print(type(x))
+print(type(a))
+print(type(b))
